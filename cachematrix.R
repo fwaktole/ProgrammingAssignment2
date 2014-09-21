c5e10cce71ecc3b9e56c47ee9a9ce33a324ef730
fwaktole <firew.waktole@yahoo.com>
2014-09-21 10:43:27 -0400

    Assignment: Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix creates list of functions (setMatrix, getMatrix,
+# cacheInverse,getInverse), which is really a list 
+# containing a function to store the matrix and cached value of the inverse
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = numeric()) {
+       # setting the initial cache to NULL
+        cache <- NULL
+        
+        # set the value of a matrix
+        setMatrix <- function(newValue) 
+        {
+        	x <<- newValue
+           	# truncating the cache
+                cache <<- NULL
+        }
 
-makeCacheMatrix <- function(x = matrix()) {
+        # get the value of a matrix
+        getMatrix <- function() {x}
 
+        # get the cached inverse value
+        cacheInverse <- function(solve) {cache <<- solve}
+
+        # get the cached value
+        getInverse <- function() {cache}
+        
+        # return list of functions
+        list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
+# this function computes the inverse of the special "matrix" returned 
+# by the above function (makeCacheMatrix)
 
-## Write a short comment describing this function
+cacheSolve <- function(y, ...) 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+	{
+        # cached value
+        inverse <- y$getInverse()
+        # check if value exists
+        if(!is.null(inverse)) {
+             message("returning the cached data")
+             # return the value
+             return(inverse)
+        }
+        # else return the matrix and compute the inverse
+        # and cache it
+        data <- y$getMatrix()
+        inverse <- solve(data)
+        y$cacheInverse(inverse)
+        
+        # getting the inverse
+        inverse
+	}
